9a17b2266e6187ea34ce73a9c05dedf0949eaf42
Sindre Moldeklev <sndrem@live.no>
2017-02-28 22:18:02 +0100

    Initial commit

---
+++ scripts-FINISHED.js
@@ -0,0 +1,69 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+const backgroundImage = document.querySelector(".background-image");
+
+function timer(seconds) {
+  // clear any existing timers
+  clearInterval(countdown);
+
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+  const maxSecondsLeft = Math.round((then - Date.now()) / 1000);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if we should stop it!
+    if(secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    // display it
+    displayTimeLeft(secondsLeft);
+    let blur = percentBetween(secondsLeft, maxSecondsLeft, 0)
+    unblur(blur)
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function percentBetween(secondsLeft, max, min) {
+  const range = max - min;
+  const startValue = secondsLeft - min;
+  const percentage = (startValue * 100) / range; 
+  return percentage;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+function unblur(blur) {
+  backgroundImage.setAttribute("style", `filter: blur(${blur}px)`);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  console.log(mins);
+  timer(mins * 60);
+  this.reset();
+});
